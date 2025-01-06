a59eb6c6e3cb9d0f1483cee48e11a8231565a71a
Shahredza <izrin96@gmail.com>
2025-01-07 05:55:05 +0800

    fix: env

--- src/env.mjs
+++ src/env.mjs
@@ -3,16 +3,11 @@ import * as z from "zod";
 
 export const env = createEnv({
   server: {
-    JWT_SECRET: z.string(),
-    RAMPER_URL: z.string().url(),
-    RAMPER_API_URL: z.string().url(),
-    RAMPER_APP_ID: z.string(),
-    RAMPER_USERAGENT: z.string(),
     INDEXER_PROXY_KEY: z.string(),
     INDEXER_PROXY_URL: z.string(),
     DB_PROXY_KEY: z.string(),
     DB_PROXY_URL: z.string(),
-    DATABASE_URL: z.string(),
+    DATABASE_URL: z.string().optional(),
   },
   client: {},
   experimental__runtimeEnv: {},
