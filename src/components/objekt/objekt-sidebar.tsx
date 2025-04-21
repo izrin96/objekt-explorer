0fe76821a29121e91fc8b4d0a7bc7ba2866e0a04
Shahredza <izrin96@gmail.com>
2025-04-21 17:17:00 +0800

    fix objekt sidebar if serial 0

--- src/components/objekt/objekt-sidebar.tsx
+++ src/components/objekt/objekt-sidebar.tsx
@@ -9,7 +9,7 @@ export default function ObjektSidebar({ collection, serial }: Props) {
   return (
     <div className="absolute @container h-full items-center w-[11%] flex gap-1.5 justify-center right-0 [writing-mode:vertical-lr] text-(--objekt-text-color) select-none">
       <span className="text-[3.4cqh] font-bold">{collection}</span>
-      {serial && <span className="font-doto font-semibold text-[3.4cqh]">#{serial}</span>}
+      {serial !== undefined && <span className="font-doto font-semibold text-[3.4cqh]">#{serial}</span>}
     </div>
   );
 }
