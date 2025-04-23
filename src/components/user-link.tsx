47fd02af449ea6867680e92f8bed3e83dba33f90
Shahredza <izrin96@gmail.com>
2025-04-23 16:43:21 +0800

    show deleted user link

--- src/components/user-link.tsx
+++ src/components/user-link.tsx
@@ -11,19 +11,24 @@ export default function UserLink({
 }) {
   return (
     <div className="inline-flex gap-2">
-      <Link href={`/@${nickname ?? address}`} prefetch={false}>
-        {nickname ?? address.substring(0, 6)}
-      </Link>
-
-      {!nickname && (
-        <Tooltip delay={0} closeDelay={0}>
-          <Tooltip.Trigger aria-label="Preview">
-            <InfoIcon className="size-3.5" />
-          </Tooltip.Trigger>
-          <Tooltip.Content>
-            Displayed as a wallet address. Nickname not yet available.
-          </Tooltip.Content>
-        </Tooltip>
+      {address ? (
+        <>
+          <Link href={`/@${nickname ?? address}`} prefetch={false}>
+            {nickname ?? address.substring(0, 6)}
+          </Link>
+          {!nickname && (
+            <Tooltip delay={0} closeDelay={0}>
+              <Tooltip.Trigger aria-label="Preview">
+                <InfoIcon className="size-3.5" />
+              </Tooltip.Trigger>
+              <Tooltip.Content>
+                Displayed as a wallet address. Nickname not yet available.
+              </Tooltip.Content>
+            </Tooltip>
+          )}
+        </>
+      ) : (
+        <span>(deleted)</span>
       )}
     </div>
   );
