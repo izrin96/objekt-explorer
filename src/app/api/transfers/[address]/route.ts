288dad2d9b430df07178a605f311df94e7cdb699
Shahredza <izrin96@gmail.com>
2025-04-27 21:24:12 +0800

    fix transfer api type 'sent' should not include spin

--- src/app/api/transfers/[address]/route.ts
+++ src/app/api/transfers/[address]/route.ts
@@ -64,7 +64,7 @@ export async function GET(
           ? [
               and(
                 eq(transfers.from, params.address.toLowerCase()),
-                ne(transfers.to, NULL_ADDRESS)
+                ne(transfers.to, SPIN_ADDRESS)
               ),
             ]
           : []),
