5dd9527d838aa510adc4ed8d9175a8dde925d2b0
Shahredza <izrin96@gmail.com>
2025-05-08 11:00:54 +0800

    remove vercel cache

--- src/app/api/common.ts
+++ src/app/api/common.ts
@@ -1,7 +1,5 @@
 export function cacheHeaders(ttl?: number) {
   return {
     "Cache-Control": "max-age=15",
-    "CDN-Cache-Control": "max-age=30",
-    "Vercel-CDN-Cache-Control": `max-age=${ttl ?? 30}, stale-while-revalidate=15`,
   };
 }
