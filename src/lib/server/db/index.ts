57d19730e66f72f6e04141862fa02f6ced54d0c0
Shahredza <izrin96@gmail.com>
2025-05-16 06:27:31 +0800

    remove pg proxy in prod

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,36 +1,12 @@
-import { drizzle as drizzleNode } from "drizzle-orm/node-postgres";
-import { drizzle } from "drizzle-orm/pg-proxy";
+import { drizzle } from "drizzle-orm/node-postgres";
 import { env } from "@/env";
 import * as schema from "./schema";
-import { ofetch } from "ofetch";
 
-export const db =
+export const db = drizzle(
   process.env.NODE_ENV === "development"
-    ? drizzleNode(process.env.DATABASE_URL_MAIN_LOCAL!, {
-        schema,
-      })
-    : drizzle(
-        async (sql, params, method) => {
-          try {
-            const rows = await ofetch(`${env.DB_PROXY_URL}/query`, {
-              retry: 1,
-              retryDelay: 500,
-              method: "POST",
-              headers: {
-                "proxy-key": env.DB_PROXY_KEY,
-              },
-              body: JSON.stringify({
-                sql,
-                params,
-                method,
-              }),
-            });
-
-            return { rows };
-          } catch (e: any) {
-            console.error("Error from Drizzle HTTP proxy: ", e);
-            return { rows: [] };
-          }
-        },
-        { schema }
-      );
+    ? process.env.DATABASE_URL_MAIN_LOCAL!
+    : env.DATABASE_URL!,
+  {
+    schema,
+  }
+);
