41c2b6b618fb1f7a73813090df9567777af23a7c
Shahredza <izrin96@gmail.com>
2025-05-25 00:01:40 +0800

    clean up unused s3 client method

--- src/lib/server/minio/index.ts
+++ src/lib/server/minio/index.ts
@@ -1,5 +1,4 @@
 import * as Minio from "minio";
-import type internal from "stream";
 import { env } from "@/env";
 
 export const s3Client = new Minio.Client({
@@ -17,63 +16,6 @@ export async function createBucketIfNotExists(bucketName: string) {
   }
 }
 
-export async function saveFileInBucket({
-  bucketName,
-  fileName,
-  file,
-}: {
-  bucketName: string;
-  fileName: string;
-  file: Buffer | internal.Readable;
-}) {
-  // Create bucket if it doesn't exist
-  await createBucketIfNotExists(bucketName);
-
-  // check if file exists
-  const fileExists = await checkFileExistsInBucket({
-    bucketName,
-    fileName,
-  });
-
-  if (fileExists) {
-    throw new Error("File already exists");
-  }
-
-  // Upload image to S3 bucket
-  await s3Client.putObject(bucketName, fileName, file);
-}
-
-export async function checkFileExistsInBucket({
-  bucketName,
-  fileName,
-}: {
-  bucketName: string;
-  fileName: string;
-}) {
-  try {
-    await s3Client.statObject(bucketName, fileName);
-  } catch (error) {
-    return false;
-  }
-  return true;
-}
-
-export async function getFileFromBucket({
-  bucketName,
-  fileName,
-}: {
-  bucketName: string;
-  fileName: string;
-}) {
-  try {
-    await s3Client.statObject(bucketName, fileName);
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-  return await s3Client.getObject(bucketName, fileName);
-}
-
 export async function deleteFileFromBucket({
   bucketName,
   fileName,
@@ -84,7 +26,6 @@ export async function deleteFileFromBucket({
   try {
     await s3Client.removeObject(bucketName, fileName);
   } catch (error) {
-    console.error(error);
     return false;
   }
   return true;
@@ -103,16 +44,4 @@ export async function createPresignedUrlToUpload({
   await createBucketIfNotExists(bucketName);
 
   return await s3Client.presignedPutObject(bucketName, fileName, expiry);
-}
-
-export async function createPresignedUrlToDownload({
-  bucketName,
-  fileName,
-  expiry = 60 * 60, // 1 hour
-}: {
-  bucketName: string;
-  fileName: string;
-  expiry?: number;
-}) {
-  return await s3Client.presignedGetObject(bucketName, fileName, expiry);
 }
