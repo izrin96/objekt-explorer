b0534c985531f620daf7301783a935201e616117
Shahredza <izrin96@gmail.com>
2025-05-27 00:01:47 +0800

    minor tab overflow fix

--- src/components/profile/profile-tabs.tsx
+++ src/components/profile/profile-tabs.tsx
@@ -7,23 +7,25 @@ import { usePathname } from "next/navigation";
 export default function ProfileTabs({ nickname }: { nickname: string }) {
   const pathname = usePathname();
   return (
-    <div className="overflow-x-auto">
-      <Tabs aria-label="Navbar" className="w-fit" selectedKey={pathname}>
-        <Tabs.List
-          items={[
-            { url: `/@${nickname}`, label: "Collection" },
-            { url: `/@${nickname}/trades`, label: "Trade History" },
-            { url: `/@${nickname}/progress`, label: "Progress" },
-            { url: `/@${nickname}/stats`, label: "Statistics" },
-          ]}
-        >
-          {(item) => (
-            <Tabs.Tab id={item.url} href={item.url}>
-              {item.label}
-            </Tabs.Tab>
-          )}
-        </Tabs.List>
-      </Tabs>
-    </div>
+    <Tabs
+      aria-label="Navbar"
+      className="overflow-x-auto"
+      selectedKey={pathname}
+    >
+      <Tabs.List
+        items={[
+          { url: `/@${nickname}`, label: "Collection" },
+          { url: `/@${nickname}/trades`, label: "Trade History" },
+          { url: `/@${nickname}/progress`, label: "Progress" },
+          { url: `/@${nickname}/stats`, label: "Statistics" },
+        ]}
+      >
+        {(item) => (
+          <Tabs.Tab id={item.url} href={item.url}>
+            {item.label}
+          </Tabs.Tab>
+        )}
+      </Tabs.List>
+    </Tabs>
   );
 }
