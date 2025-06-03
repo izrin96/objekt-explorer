394ec671ddc24b79041c81028e72ed576845319e
Shahredza <izrin96@gmail.com>
2025-06-03 17:25:48 +0800

    Revert "minor css fix"
    This reverts commit f05ebd7c73246d6e8cbd4c62685003ea0f35fc18.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
     >
       <body
         className={cn(
-          "min-h-svh antialiased relative",
+          "min-h-svh antialiased",
           process.env.NODE_ENV === "development" ? "debug-screens" : ""
         )}
       >
