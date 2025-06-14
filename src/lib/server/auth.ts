daf2c431b7d3eaaf9c67cc2e5d41382f1e70d831
Shahredza <izrin96@gmail.com>
2025-06-14 13:41:18 +0800

    disable email verification

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -29,7 +29,7 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
-    requireEmailVerification: true,
+    requireEmailVerification: false,
     sendResetPassword: async ({ user, url }) => {
       await sendResetPassword(user.email, url);
     },
