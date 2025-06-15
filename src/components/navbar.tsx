ab16bdf00fc7218f92220d019db52a816ed0c984
Shahredza <izrin96@gmail.com>
2025-06-15 17:44:56 +0800

    temporary hide live page from nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
           </Link>
           <Changelog />
           <Activity />
-          <Live />
+          {/* <Live /> */}
         </div>
         <div className="flex items-center gap-2">
           <ThemeSwitcher />
