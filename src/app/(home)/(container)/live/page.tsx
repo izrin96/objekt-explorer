df0893891aa9c6b60c802490bd27f8cfa72db08a
Shahredza <izrin96@gmail.com>
2025-06-19 19:51:25 +0800

    fix build

--- src/app/(home)/(container)/live/page.tsx
+++ src/app/(home)/(container)/live/page.tsx
@@ -13,9 +13,9 @@ export default async function Page() {
   return (
     <div className="flex flex-col pb-36 pt-2">
       <Note>
-        As this feature violates Cosmo's Terms of Service, we will no longer
-        continue offering it. Please watch the live stream on the Cosmo app
-        instead.
+        As this feature violates Cosmo&apos;s Terms of Service, we will no
+        longer continue offering it. Please watch the live stream on the Cosmo
+        app instead.
       </Note>
       {/* <LiveSessionList /> */}
     </div>
