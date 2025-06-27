4dc64100fd6d5b68f8905ab128bfb23ab76bb8bd
Shahredza <izrin96@gmail.com>
2025-06-27 17:21:18 +0800

    objekt sidebar serial make it readable

--- src/components/objekt/objekt-sidebar.tsx
+++ src/components/objekt/objekt-sidebar.tsx
@@ -10,9 +10,10 @@ export default function ObjektSidebar({ collection, serial }: Props) {
     <div className="absolute @container h-full items-center w-[11%] flex gap-1.5 justify-center right-0 [writing-mode:vertical-lr] text-(--objekt-text-color) select-none">
       <span className="text-[3.4cqh] font-bold">{collection}</span>
       {serial !== undefined && (
-        <span className="font-dot text-[3.4cqh] font-medium pr-[0.6cqh]">
-          #{serial}
-        </span>
+        <div className="text-[3.4cqh] font-light tabular-nums gap-0.5 flex">
+          <span>#</span>
+          <span>{serial}</span>
+        </div>
       )}
     </div>
   );
