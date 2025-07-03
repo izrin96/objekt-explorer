48c6f18f3353a04ec47f33eff6375554e4feb956
Shahredza <izrin96@gmail.com>
2025-07-03 16:26:15 +0800

    clean up

--- src/hooks/use-profile.tsx
+++ src/hooks/use-profile.tsx
@@ -3,19 +3,14 @@
 import { createContext, type PropsWithChildren, useContext, useRef } from "react";
 import { createStore, type StoreApi, useStore } from "zustand";
 import type { PublicList } from "@/lib/server/api/routers/list";
-import type { PinObjekt } from "@/lib/universal/objekts";
 import type { PublicProfile } from "@/lib/universal/user";
 
 type ProfileProps = {
   profile: PublicProfile | undefined;
   list: PublicList | undefined;
-  pins: PinObjekt[];
 };
 
-interface ProfileState extends ProfileProps {
-  addPin: (pin: PinObjekt) => void;
-  removePin: (tokenId: string) => void;
-}
+interface ProfileState extends ProfileProps {}
 
 const ProfileContext = createContext<StoreApi<ProfileState> | null>(null);
 
@@ -25,22 +20,11 @@ const createProfileStore = (initial: Partial<ProfileProps>) => {
   const DEFAULT_PROPS: ProfileProps = {
     profile: undefined,
     list: undefined,
-    pins: [],
   };
 
-  return createStore<ProfileState>()((set) => ({
+  return createStore<ProfileState>()(() => ({
     ...DEFAULT_PROPS,
     ...initial,
-    addPin: (pin: PinObjekt) =>
-      set((state) => ({
-        ...state,
-        pins: [pin, ...state.pins],
-      })),
-    removePin: (tokenId: string) =>
-      set((state) => ({
-        ...state,
-        pins: state.pins.filter((p) => p.tokenId !== tokenId),
-      })),
   }));
 };
 
