62a2d95319519033371c287dc433e455d81602ad
Shahredza <izrin96@gmail.com>
2025-07-07 17:28:18 +0800

    change objekt sidebar font size

--- src/components/objekt/objekt-sidebar.tsx
+++ src/components/objekt/objekt-sidebar.tsx
@@ -5,11 +5,11 @@ type Props = {
 
 export default function ObjektSidebar({ collection, serial }: Props) {
   return (
-    <div className="@container absolute right-0 flex h-full w-[11%] select-none items-center justify-center gap-1.5 text-(--objekt-text-color) [writing-mode:vertical-lr]">
-      <span className="font-bold text-[3.4cqh]">{collection}</span>
+    <div className="@container absolute right-0 flex h-full w-[11%] select-none items-center justify-center text-(--objekt-text-color) [writing-mode:vertical-lr]">
+      <span className="font-semibold text-[4cqh]">{collection}</span>
       {serial !== undefined && (
-        <div className="flex gap-0.5 font-light text-[3.4cqh] tabular-nums">
-          <span>#</span>
+        <div className="flex pt-[.5em] font-medium text-[4cqh]">
+          <span className="pb-[.1em]">#</span>
           <span>{serial}</span>
         </div>
       )}
