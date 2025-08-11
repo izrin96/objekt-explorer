9aa5d230778eff49ab0e0501363bb5eee19052b2
Shahredza <izrin96@gmail.com>
2025-08-12 05:44:37 +0800

    add es2022 es2023 polyfill

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,11 @@
+// ES2022
+import "core-js/es/array/at";
+import "core-js/es/object/has-own";
+import "core-js/es/array/find-last";
+import "core-js/es/array/find-last-index";
+
+// ES2023
+import "core-js/es/array/to-sorted";
+import "core-js/es/array/to-reversed";
+import "core-js/es/array/to-spliced";
+import "core-js/es/array/with";
