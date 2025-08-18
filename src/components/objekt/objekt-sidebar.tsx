333baced0bc7c71941fa674cd88c8b70a17c5864
Shahredza <izrin96@gmail.com>
2025-08-18 16:21:09 +0800

    implement idntt special class gradient objekt band

--- src/components/objekt/objekt-sidebar.tsx
+++ src/components/objekt/objekt-sidebar.tsx
@@ -1,6 +1,7 @@
 import type { CSSProperties } from "react";
 import { useElementSize } from "@/hooks/use-element-size";
 import type { ValidObjekt } from "@/lib/universal/objekts";
+import { cn } from "@/utils/classes";
 import IdnttLogo from "../idntt-logo";
 
 type Props = {
@@ -24,7 +25,13 @@ export default function ObjektSidebar({ objekt, hideSerial = false }: Props) {
       }
     >
       {objekt.artist === "idntt" && (
-        <div className="flex h-[88%] w-full items-center justify-between rounded-l-(--band-radius) bg-(--objekt-bg-color) px-(--band-spacing) font-bold [writing-mode:vertical-lr]">
+        <div
+          className={cn(
+            "flex h-[88%] w-full items-center justify-between rounded-l-(--band-radius) bg-(--objekt-bg-color) px-(--band-spacing) font-bold [writing-mode:vertical-lr]",
+            objekt.class === "Special" &&
+              "bg-[radial-gradient(circle_at_top_left,_#F9E7E3,_#CDD3ED,_#F3E0D1,_#F0E0E3,_#F3D7E5,_#EBD4E8,_#EBB7DD,_#D0B4EA)]",
+          )}
+        >
           <span>{objekt.member}</span>
           <IdnttLogo className="h-[10%] w-[120%] rotate-90" />
         </div>
