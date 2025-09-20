2f302ef8368266a3f1f86a31e33c4e93d52fe9bc
Shahredza <izrin96@gmail.com>
2025-09-21 05:08:33 +0800

    refactor: indexer use proxy in dev

--- src/lib/server/db/indexer/index.ts
+++ src/lib/server/db/indexer/index.ts
@@ -1,36 +1,30 @@
-import { drizzle as drizzleNode } from "drizzle-orm/node-postgres";
 import { drizzle } from "drizzle-orm/pg-proxy";
 import { ofetch } from "ofetch";
 import { env } from "@/env";
 import * as schema from "./schema";
 
-export const indexer =
-  process.env.NODE_ENV === "development"
-    ? drizzleNode(process.env.DATABASE_URL_INDEXER_LOCAL!, {
-        schema,
-      })
-    : drizzle(
-        async (sql, params, method) => {
-          try {
-            const rows = await ofetch(`${env.INDEXER_PROXY_URL}/query`, {
-              retry: false,
-              timeout: 10000,
-              method: "POST",
-              headers: {
-                "proxy-key": env.INDEXER_PROXY_KEY,
-              },
-              body: JSON.stringify({
-                sql,
-                params,
-                method,
-              }),
-            });
-
-            return { rows };
-          } catch (e: any) {
-            console.error("Error from Drizzle HTTP proxy: ", e);
-            return { rows: [] };
-          }
+export const indexer = drizzle(
+  async (sql, params, method) => {
+    try {
+      const rows = await ofetch(`${env.INDEXER_PROXY_URL}/query`, {
+        retry: false,
+        timeout: 10000,
+        method: "POST",
+        headers: {
+          "proxy-key": env.INDEXER_PROXY_KEY,
         },
-        { schema },
-      );
+        body: JSON.stringify({
+          sql,
+          params,
+          method,
+        }),
+      });
+
+      return { rows };
+    } catch (e: any) {
+      console.error("Error from Drizzle HTTP proxy: ", e);
+      return { rows: [] };
+    }
+  },
+  { schema },
+);
