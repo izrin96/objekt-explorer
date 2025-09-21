e74bafa176156ad74a872f1bb662a365ee2f1a0c
Shahredza <izrin96@gmail.com>
2025-09-22 03:25:05 +0800

    refactor: simplify filter-wide by removing media query

--- src/components/filters/filter-wide.tsx
+++ src/components/filters/filter-wide.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import { useIsClient, useMediaQuery } from "usehooks-ts";
+import { useIsClient } from "usehooks-ts";
 import { useWide } from "@/hooks/use-wide";
 import { Toggle } from "../ui";
 
 export default function WideFilter() {
   const { wide, setWide } = useWide();
   const isClient = useIsClient();
-  const isMax = useMediaQuery("(min-width: 1280px)");
-  if (!isClient || !isMax) return;
+  if (!isClient) return;
 
   return (
-    <Toggle intent="outline" isSelected={wide} onChange={setWide}>
+    <Toggle intent="outline" isSelected={wide} onChange={setWide} className="hidden xl:block">
       {wide ? "Compact" : "Wide"}
     </Toggle>
   );
