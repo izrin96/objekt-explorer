ccce1451684e6cf4e333eac26dd58d96dbcc38a5
Shahredza <izrin96@gmail.com>
2025-09-28 02:06:21 +0800

    style: toast component theming

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -16,6 +16,29 @@ const Toast = ({ ...props }: ToasterProps) => {
         className:
           "*:data-icon:self-start font-sans has-data-description:*:data-icon:mt-1 *:data-icon:mt-0.5 backdrop-blur-2xl",
       }}
+      style={
+        {
+          "--normal-bg": "var(--overlay)",
+          "--normal-text": "var(--overlay-fg)",
+          "--normal-border": "var(--border)",
+
+          "--success-bg": "var(--overlay)",
+          "--success-text": "var(--overlay-fg)",
+          "--success-border": "var(--border)",
+
+          "--error-bg": "var(--overlay)",
+          "--error-text": "var(--overlay-fg)",
+          "--error-border": "var(--border)",
+
+          "--warning-bg": "var(--overlay)",
+          "--warning-text": "var(--overlay-fg)",
+          "--warning-border": "var(--border)",
+
+          "--info-bg": "var(--overlay)",
+          "--info-text": "var(--overlay-fg)",
+          "--info-border": "var(--border)",
+        } as React.CSSProperties
+      }
       {...props}
     />
   );
