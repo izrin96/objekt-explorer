954c248cb9363a9a9cbb8ea8c580d0d1fc316592
Shahredza <izrin96@gmail.com>
2025-09-27 22:50:34 +0800

    fix: font not load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,7 +96,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
     >
       <body
         className={cn(
-          "min-h-svh antialiased",
+          "min-h-svh font-sans antialiased",
           process.env.NODE_ENV === "development" ? "debug-screens" : "",
         )}
       >
