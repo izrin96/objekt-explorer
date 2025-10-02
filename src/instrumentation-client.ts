87b77de5ada3e52e113bcb28d49f22ffedb587bd
Shahredza <izrin96@gmail.com>
2025-10-03 03:56:29 +0800

    chore: remove unused polyfills

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,11 +1,3 @@
-// ES2022
 import "core-js/es/array/at";
-import "core-js/es/object/has-own";
-import "core-js/es/array/find-last";
-import "core-js/es/array/find-last-index";
-
-// ES2023
 import "core-js/es/array/to-sorted";
 import "core-js/es/array/to-reversed";
-import "core-js/es/array/to-spliced";
-import "core-js/es/array/with";
