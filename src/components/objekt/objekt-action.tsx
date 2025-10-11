c45ea94c7b3a117ac5e15611008d0f31e26dde67
Shahredza <izrin96@gmail.com>
2025-10-12 05:20:49 +0800

    fix: button too small for mobile

--- src/components/objekt/objekt-action.tsx
+++ src/components/objekt/objekt-action.tsx
@@ -4,18 +4,11 @@ import {
   CheckIcon,
   DotsThreeVerticalIcon,
   LockSimpleIcon,
-  LockSimpleOpenIcon,
   PushPinIcon,
-  PushPinSlashIcon,
 } from "@phosphor-icons/react/dist/ssr";
 import type { PropsWithChildren } from "react";
-import { useBatchLock } from "@/hooks/actions/batch-lock";
-import { useBatchPin } from "@/hooks/actions/batch-pin";
-import { useBatchUnlock } from "@/hooks/actions/batch-unlock";
-import { useBatchUnpin } from "@/hooks/actions/batch-unpin";
 import { useObjektSelect } from "@/hooks/use-objekt-select";
 import type { ValidObjekt } from "@/lib/universal/objekts";
-import type { PublicProfile } from "@/lib/universal/user";
 import { cn } from "@/utils/classes";
 import { Button } from "../ui/button";
 import { Menu, MenuContent } from "../ui/menu";
@@ -30,7 +23,7 @@ export function ObjektSelect({ objekt }: { objekt: ValidObjekt }) {
       className={cn("hidden bg-bg/80 px-2 text-fg group-hover:block", isSelected && "block")}
       onClick={() => objektSelect(objekt)}
     >
-      <CheckIcon className="size-2 sm:size-3" weight="bold" />
+      <CheckIcon className="size-3" weight="bold" />
     </Button>
   );
 }
@@ -45,88 +38,6 @@ export function ObjektOverlay({ isPin, isLocked }: { isPin: boolean; isLocked: b
   );
 }
 
-export function ObjektTogglePin({
-  profile,
-  isPin,
-  tokenId,
-}: {
-  profile: PublicProfile;
-  isPin: boolean;
-  tokenId: string;
-}) {
-  const pin = useBatchPin();
-  const unpin = useBatchUnpin();
-  return (
-    <Button
-      size="sq-xs"
-      intent="plain"
-      className="bg-bg/80"
-      onClick={() => {
-        if (isPin) {
-          unpin.mutate({
-            address: profile.address,
-            tokenIds: [Number(tokenId)],
-          });
-        } else {
-          pin.mutate({
-            address: profile.address,
-            tokenIds: [Number(tokenId)],
-          });
-        }
-      }}
-    >
-      <span className="text-nowrap font-semibold text-xs">
-        {isPin ? (
-          <PushPinSlashIcon className="size-2 sm:size-3" />
-        ) : (
-          <PushPinIcon className="size-2 sm:size-3" />
-        )}
-      </span>
-    </Button>
-  );
-}
-
-export function ObjektToggleLock({
-  profile,
-  isLocked,
-  tokenId,
-}: {
-  profile: PublicProfile;
-  isLocked: boolean;
-  tokenId: string;
-}) {
-  const lock = useBatchLock();
-  const unlock = useBatchUnlock();
-  return (
-    <Button
-      size="sq-xs"
-      intent="plain"
-      className="bg-bg/80"
-      onClick={() => {
-        if (isLocked) {
-          unlock.mutate({
-            address: profile.address,
-            tokenIds: [Number(tokenId)],
-          });
-        } else {
-          lock.mutate({
-            address: profile.address,
-            tokenIds: [Number(tokenId)],
-          });
-        }
-      }}
-    >
-      <span className="text-nowrap font-semibold text-xs">
-        {isLocked ? (
-          <LockSimpleOpenIcon className="size-2 sm:size-3" />
-        ) : (
-          <LockSimpleIcon className="size-2 sm:size-3" />
-        )}
-      </span>
-    </Button>
-  );
-}
-
 export function ObjektHoverMenu({ children }: PropsWithChildren) {
   return (
     <Menu>
@@ -135,7 +46,7 @@ export function ObjektHoverMenu({ children }: PropsWithChildren) {
         intent="plain"
         className="hidden bg-bg/80 px-2 text-fg group-hover:block data-pressed:block"
       >
-        <DotsThreeVerticalIcon className="size-2 sm:size-3" weight="bold" />
+        <DotsThreeVerticalIcon className="size-3" weight="bold" />
       </Button>
       <MenuContent placement="bottom right">{children}</MenuContent>
     </Menu>
