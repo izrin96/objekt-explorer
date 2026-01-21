e9f3b6b15e393626fd1c8e36a3b5af7825a76b0b
Shahredza <izrin96@gmail.com>
2026-01-22 04:16:01 +0800

    change collection filter component to normal select

--- apps/web/src/components/filters/filter-collection.tsx
+++ apps/web/src/components/filters/filter-collection.tsx
@@ -2,13 +2,17 @@
 
 import { useTranslations } from "next-intl";
 import { useCallback } from "react";
-import type { Key } from "react-aria-components";
+import { Autocomplete, type Key, Popover, useFilter } from "react-aria-components";
 import { useFilterData } from "@/hooks/use-filter-data";
 import { useFilters } from "@/hooks/use-filters";
-import { MultipleSelect, MultipleSelectContent, MultipleSelectItem } from "../ui/multiple-select";
+import { Dialog } from "../ui/dialog";
+import { ListBox } from "../ui/list-box";
+import { SearchField, SearchInput } from "../ui/search-field";
+import { Select, SelectItem, SelectTrigger } from "../ui/select";
 
 export default function CollectionFilter() {
   const t = useTranslations("filter");
+  const { contains } = useFilter({ sensitivity: "base" });
   const { collections } = useFilterData();
   const [filters, setFilters] = useFilters();
   const selected = filters.collection ?? [];
@@ -20,20 +24,33 @@ export default function CollectionFilter() {
   }, []);
 
   return (
-    <MultipleSelect
-      className="min-w-52 max-w-min"
-      placeholder={t("collection_no")}
-      aria-label={t("collection_no")}
+    <Select
+      selectionMode="multiple"
       value={selected}
       onChange={update}
+      placeholder={t("collection_no")}
+      aria-label={t("collection_no")}
+      className="max-w-52"
     >
-      <MultipleSelectContent items={collections.map((name) => ({ id: name, name }))}>
-        {(item) => (
-          <MultipleSelectItem id={item.id} textValue={item.name}>
-            {item.name}
-          </MultipleSelectItem>
-        )}
-      </MultipleSelectContent>
-    </MultipleSelect>
+      <SelectTrigger />
+      <Popover className="entering:fade-in exiting:fade-out flex max-h-80 w-(--trigger-width) entering:animate-in exiting:animate-out flex-col overflow-hidden rounded-lg border bg-overlay">
+        <Dialog aria-label="Language">
+          <Autocomplete filter={contains}>
+            <div className="border-b bg-muted p-2">
+              <SearchField className="rounded-lg bg-bg" autoFocus>
+                <SearchInput />
+              </SearchField>
+            </div>
+            <ListBox className="max-h-[inherit] min-w-[inherit] rounded-t-none border-0 bg-transparent shadow-none">
+              {collections.map((collection) => (
+                <SelectItem id={collection} key={collection} textValue={collection}>
+                  {collection}
+                </SelectItem>
+              ))}
+            </ListBox>
+          </Autocomplete>
+        </Dialog>
+      </Popover>
+    </Select>
   );
 }
