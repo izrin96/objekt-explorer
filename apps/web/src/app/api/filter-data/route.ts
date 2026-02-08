bfe1ee569b3d9584284a3d10036f7bb2c2fa6efd
Shahredza <izrin96@gmail.com>
2026-02-09 00:19:28 +0800

    clean up

--- apps/web/src/app/api/filter-data/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-import { fetchFilterData } from "@/lib/server/objekt";
-
-export async function GET() {
-  const result = await fetchFilterData();
-  return NextResponse.json(result);
-}
