598f4db7fca097159cf1f7bce9aa95bcfcac1f11
Shahredza <izrin96@gmail.com>
2026-02-10 18:12:55 +0800

    minor fix

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -14,6 +14,7 @@ export const OBJEKT_SIZE = {
 export const validColumns = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
 export function parseNickname(address: string, nickname?: string | null) {
+  if (nickname?.toLowerCase() === address.toLowerCase()) return `${address.substring(0, 8)}...`;
   return nickname ?? `${address.substring(0, 8)}...`;
 }
 
