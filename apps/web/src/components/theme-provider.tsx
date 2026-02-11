2e4ceedaded04b9a0109dffe88ca334920ca73c4
Shahredza <izrin96@gmail.com>
2026-02-11 14:46:51 +0800

    update theme provider

--- apps/web/src/components/theme-provider.tsx
+++ apps/web/src/components/theme-provider.tsx
@@ -3,7 +3,18 @@
 import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
 
 const ThemeProvider = ({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) => {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      enableColorScheme
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 };
 
 export { useTheme, ThemeProvider };
