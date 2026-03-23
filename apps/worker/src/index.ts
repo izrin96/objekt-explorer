eb612bff4009a6ce73f2dbc56cde66f014135e74
Shahredza <izrin96@gmail.com>
2026-03-23 20:03:56 +0800

    add some comment [skip ci]

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -5,24 +5,28 @@ import { updateTransferableCosmoSpin } from "./job/cosmo-spin";
 import { populateSerialOffline } from "./job/populate-serial";
 import { cleanupProfileLists, syncProfileListsToCache } from "./job/profile-list-cleanup";
 
-// 1. fix metadata (refetch metadata from endpoint)
+// 1. fix metadata
+// refetch metadata from endpoint
+// fallback to v3 if v1 fail
 await fixEmptyCollection();
 await fixObjektSerialZero();
 
-// 2. populate missing serials (case where metadata is not found)
-
-// a) for online objekts
-// note: temporary disable due to v1 endpoint is still up
+// 2. populate missing serials for online objekts
+// case where metadata is not found
+// required objekt start from serial 1, should not run on Atom01 era as they have a real serial 0 objekt
+// temporary disable due to v1 endpoint is still up
 // await populateSerial();
 
-// b) for offline objekts
+// 3. populate missing serials for offline objekts
+// case where metadata is not found
+// required neighbour serial to be found to predict serial
 await populateSerialOffline();
 
-// 3. cosmo-spin transferable update
+// 4. cosmo-spin transferable update
 await updateTransferableCosmoSpin();
 void new Cron("*/10 * * * *", updateTransferableCosmoSpin);
 
-// 4. profile list
+// 5. profile list
 await syncProfileListsToCache();
 void new Cron("*/30 * * * *", syncProfileListsToCache);
 
