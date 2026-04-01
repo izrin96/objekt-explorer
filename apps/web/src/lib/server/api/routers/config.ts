cd9779dc3635cb68074ff1fd1b9b308727ca3455
Shahredza <izrin96@gmail.com>
2026-04-01 22:55:19 +0800

    set expires cookie, better-auth minimal import

--- apps/web/src/lib/server/api/routers/config.ts
+++ apps/web/src/lib/server/api/routers/config.ts
@@ -17,7 +17,7 @@ export const configRouter = {
   setArtists: pub.input(z.enum(validArtists).array()).handler(async ({ input: artists }) => {
     const cookie = await cookies();
     cookie.set("artists", JSON.stringify(artists), {
-      maxAge: 60 * 60 * 24 * 30,
+      maxAge: 12 * 60 * 60 * 24 * 30,
       sameSite: "lax",
       httpOnly: true,
       secure: true,
--- apps/web/src/lib/server/locale.ts
+++ apps/web/src/lib/server/locale.ts
@@ -11,5 +11,7 @@ export async function getUserLocale() {
 }
 
 export async function setUserLocale(locale: Locale) {
-  (await cookies()).set(COOKIE_NAME, locale);
+  (await cookies()).set(COOKIE_NAME, locale, {
+    maxAge: 12 * 60 * 60 * 24 * 30,
+  });
 }
