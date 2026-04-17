fd2cc04ae349c48b338f5b76af5acdac073ec0ee
Shahredza <izrin96@gmail.com>
2026-04-18 05:08:48 +0800

    set viewport

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 import "@/app/globals.css";
 import { PublicEnvScript } from "next-runtime-env";
@@ -40,6 +40,12 @@ const notoSansSc = Noto_Sans_SC({
   weight: "variable",
   subsets: ["latin"],
 });
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
 
 export const metadata: Metadata = {
   title: {
