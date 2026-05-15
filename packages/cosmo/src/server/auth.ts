61885bdd8c53a4d8e4d3737442e0d50ce969af77
Shahredza <izrin96@gmail.com>
2026-05-15 10:18:56 +0800

    merge cosmo package auth.ts from apollo/cosmo-web

--- packages/cosmo/src/server/auth.ts
+++ packages/cosmo/src/server/auth.ts
@@ -1,12 +1,43 @@
 import type { RefreshTokenResult } from "../types/auth";
+import { encrypt, EncryptionError } from "./encryption";
 import { cosmo } from "./http";
 
 /**
  * Refresh the given token.
+ * @deprecated use refreshV3
  */
-export async function refresh(refreshToken: string): Promise<RefreshTokenResult> {
+export async function refresh(
+  refreshToken: string,
+  signal: AbortSignal | null = null,
+): Promise<RefreshTokenResult> {
   return await cosmo<{ credentials: RefreshTokenResult }>("/auth/v1/refresh", {
     method: "POST",
     body: { refreshToken },
+    signal,
+  }).then((res: { credentials: RefreshTokenResult }) => res.credentials);
+}
+
+/**
+ * Refresh the given token.
+ */
+export async function refreshV3(
+  refreshToken: string,
+  key: string,
+  signal: AbortSignal | null = null,
+): Promise<RefreshTokenResult> {
+  try {
+    var body = encrypt(JSON.stringify({ refreshToken }), key);
+  } catch (err) {
+    throw new EncryptionError("Error encrypting payload", { cause: err });
+  }
+
+  return await cosmo<{ credentials: RefreshTokenResult }>("/bff/v3/users/refresh-access-token", {
+    method: "POST",
+    body,
+    headers: {
+      "Content-Type": "text/plain",
+      "x-cosmo-encrypted": "1",
+    },
+    signal,
   }).then((res: { credentials: RefreshTokenResult }) => res.credentials);
 }
