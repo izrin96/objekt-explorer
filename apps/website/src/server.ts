a230df712f0d1753627cc8dfa8e256ec566b6922
Shahredza <izrin96@gmail.com>
2026-05-29 23:54:32 +0800

    add cache control

--- apps/website/src/server.ts
+++ apps/website/src/server.ts
@@ -4,6 +4,10 @@ import { paraglideMiddleware } from "./paraglide/server.js";
 
 export default {
   fetch(request: Request) {
-    return paraglideMiddleware(request, () => handler.fetch(request));
+    return paraglideMiddleware(request, async () => {
+      const response = await handler.fetch(request);
+      response.headers.set("Cache-Control", "public, max-age=0, must-revalidate");
+      return response;
+    });
   },
 };
