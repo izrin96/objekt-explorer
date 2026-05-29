05920ef6a280a9c4edc2105c3219a2320a4a8de0
Shahredza <izrin96@gmail.com>
2026-05-30 00:47:34 +0800

    revert middleware change

--- apps/website/src/server.ts
+++ apps/website/src/server.ts
@@ -4,10 +4,6 @@ import { paraglideMiddleware } from "./paraglide/server.js";
 
 export default {
   fetch(request: Request) {
-    return paraglideMiddleware(request, async () => {
-      const response = await handler.fetch(request);
-      response.headers.set("Cache-Control", "public, max-age=0, must-revalidate");
-      return response;
-    });
+    return paraglideMiddleware(request, () => handler.fetch(request));
   },
 };
