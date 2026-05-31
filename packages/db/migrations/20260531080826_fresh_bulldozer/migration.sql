d75e2074f67254d459f0bd276d747dd6d6be4133
Shahredza <izrin96@gmail.com>
2026-05-31 16:09:52 +0800

    set notNull() on boolean, update auth-schema

---
+++ packages/db/migrations/20260531080826_fresh_bulldozer/migration.sql
@@ -0,0 +1,22 @@
+ALTER TABLE "account" ALTER COLUMN "created_at" SET DEFAULT now();--> statement-breakpoint
+ALTER TABLE "account" ALTER COLUMN "updated_at" SET DEFAULT now();--> statement-breakpoint
+ALTER TABLE "lists" ALTER COLUMN "hide_user" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "session" ALTER COLUMN "created_at" SET DEFAULT now();--> statement-breakpoint
+ALTER TABLE "session" ALTER COLUMN "updated_at" SET DEFAULT now();--> statement-breakpoint
+ALTER TABLE "user" ALTER COLUMN "email_verified" SET DEFAULT false;--> statement-breakpoint
+ALTER TABLE "user" ALTER COLUMN "created_at" SET DEFAULT now();--> statement-breakpoint
+ALTER TABLE "user" ALTER COLUMN "updated_at" SET DEFAULT now();--> statement-breakpoint
+ALTER TABLE "user" ALTER COLUMN "show_social" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "user_address" ALTER COLUMN "hide_user" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "user_address" ALTER COLUMN "private_profile" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "user_address" ALTER COLUMN "private_serial" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "user_address" ALTER COLUMN "hide_transfer" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "user_address" ALTER COLUMN "hide_nickname" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "user_address" ALTER COLUMN "is_abstract" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "verification" ALTER COLUMN "created_at" SET DEFAULT now();--> statement-breakpoint
+ALTER TABLE "verification" ALTER COLUMN "created_at" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "verification" ALTER COLUMN "updated_at" SET DEFAULT now();--> statement-breakpoint
+ALTER TABLE "verification" ALTER COLUMN "updated_at" SET NOT NULL;--> statement-breakpoint
+CREATE INDEX "account_userId_idx" ON "account" ("user_id");--> statement-breakpoint
+CREATE INDEX "session_userId_idx" ON "session" ("user_id");--> statement-breakpoint
+CREATE INDEX "verification_identifier_idx" ON "verification" ("identifier");
