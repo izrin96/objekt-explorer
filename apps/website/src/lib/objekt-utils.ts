335207e449533ee6096a11be9f4d6d496cabf8d0
Shahredza <izrin96@gmail.com>
2026-07-07 17:23:17 +0800

    update member short name

--- apps/website/src/lib/objekt-utils.ts
+++ apps/website/src/lib/objekt-utils.ts
@@ -28,8 +28,10 @@ export function getCollectionShortId(objekt: ValidObjekt) {
 function makeCollectionTags(objekt: ValidObjekt) {
   const seasonCode = objekt.season.charAt(0);
   const seasonNumber = objekt.season.slice(-2);
-  const seasonCodeRepeated = seasonCode.repeat(Number(seasonNumber));
-  const collectionNoSliced = objekt.collectionNo.slice(0, -1);
+  const seasonInt = Number(seasonNumber);
+  const seasonCodeRepeat = seasonCode.repeat(seasonInt);
+  const collectionNoSlice = objekt.collectionNo.slice(0, -1);
+  const season = objekt.season.slice(0, -2);
 
   const tags = [
     ...getMemberShortKeys(objekt.member),
@@ -38,17 +40,18 @@ function makeCollectionTags(objekt: ValidObjekt) {
     ...(objekt.artist === "idntt"
       ? []
       : [
-          `${seasonCodeRepeated}${objekt.collectionNo}`, // a201z, aa201z
-          `${seasonCodeRepeated}${collectionNoSliced}`, // a201, aa201
+          `${seasonCodeRepeat}${objekt.collectionNo}`, // a201z, aa201z
+          `${seasonCodeRepeat}${collectionNoSlice}`, // a201, aa201
         ]),
-    collectionNoSliced, // 201
+    collectionNoSlice, // 201
     objekt.member,
     objekt.class, // special
     `${objekt.class.charAt(0)}co`, // sco
     objekt.season, // atom01
-    objekt.season.slice(0, -2), // atom
+    season, // atom
+    season + seasonInt, // atom1
     seasonCode + seasonNumber, // a01
-    seasonCode + Number(seasonNumber), // a1
+    seasonCode + seasonInt, // a1
   ];
 
   // For combined members ("S8 X S12"), also index individual names
@@ -75,56 +78,84 @@ export function isObjektOwned(objekt: ValidObjekt): objekt is OwnedObjekt {
 
 // Member shortform aliases
 const shortformMembers: Record<string, string> = {
-  naky: "NaKyoung",
-  n: "Nien",
+  // triples
+  sy: "SeoYeon",
+  ham: "SeoYeon",
+  hr: "HyeRin",
+  jw: "JiWoo",
+  cy: "ChaeYeon",
+  yy: "YooYeon",
+  sm: "SooMin",
   nk: "NaKyoung",
+  naky: "NaKyoung",
+  yb: "YuBin",
+  yubam: "YuBin",
+  k: "Kaede",
+  kd: "Kaede",
+  dh: "DaHyun",
+  soda: "DaHyun",
+  ktn: "Kotone",
   tone: "Kotone",
-  sulin: "Sullin",
-  s: "Sullin",
+  yj: "YeonJi",
+  kwak: "YeonJi",
+  n: "Nien",
+  ni: "Nien",
   sh: "SoHyun",
+  ssaem: "SoHyun",
+  park: "SoHyun",
+  x: "Xinyu",
+  xn: "Xinyu",
+  m: "Mayu",
+  my: "Mayu",
+  l: "Lynn",
+  ln: "Lynn",
+  jb: "JooBin",
+  jbn: "JooBin",
+  hy: "HaYeon",
+  so: "ShiOn",
+  sion: "ShiOn",
+  cw: "ChaeWon",
+  s: "Sullin",
+  sl: "Sullin",
+  sulin: "Sullin",
+  sa: "SeoAh",
+  jy: "JiYeon",
+
+  // artms
+  hj: "HeeJin",
+  hs: "HaSeul",
+  kl: "KimLip",
+  js: "JinSoul",
   c: "Choerry",
   ch: "Choerry",
   choery: "Choerry",
-  cw: "ChaeWon",
-  cy: "ChaeYeon",
-  sy: "SeoYeon",
-  sm: "SooMin",
-  so: "ShiOn",
-  sa: "SeoAh",
-  sl: "Sullin",
-  jw: "JiWoo",
-  jb: "JooBin",
-  jy: "JiYeon",
-  js: "JinSoul",
-  dh: "DaHyun",
-  kd: "Kaede",
-  kl: "KimLip",
-  k: "Kaede",
-  hr: "HyeRin",
-  hy: "HaYeon",
-  hj: "HeeJin",
-  hs: "HaSeul",
-  yb: "YuBin",
-  yj: "YeonJi",
-  yy: "YooYeon",
-  x: "Xinyu",
-  m: "Mayu",
-  l: "Lynn",
-  soda: "DaHyun",
-  kwak: "YeonJi",
-  yubam: "YuBin",
-  ham: "SeoYeon",
-  ssaem: "SoHyun",
-  park: "SoHyun",
+
+  // idntt
+  dhn: "DoHun",
+  heju: "HeeJu",
   mg: "MinGyeol",
-  hh: "HwanHee",
-  jh: "JuHo",
   ti: "TaeIn",
+  jae: "JaeYoung",
+  jyg: "JaeYoung",
+  jyoung: "JaeYoung",
+  jh: "JuHo",
+  jwn: "JiWoon",
+  jiwon: "JiWoon",
+  hh: "HwanHee",
   cm: "CheongMyeong",
   t: "Towa",
+  tw: "Towa",
   kh: "KyuHyuk",
   nr: "NuRi",
   sj: "SeongJun",
+  yjn: "YeJoon",
+  yejon: "YeJoon",
   gb: "GyeongBeen",
   es: "EunSoo",
+  gw: "GiWoong",
+  jhn: "JooHeon",
+  joheon: "JooHeon",
+  gh: "GyungHo",
+  ec: "EunChan",
+  esg: "EunSung",
 };
